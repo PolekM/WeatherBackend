8c1235fb1ea7ecf323d1eff16a73bd2aa2244c12
PolekM <marek.pawel.polek@gmail.com>
2024-11-24 20:43:42 +0100

    jar file

--- src/main/java/com/weather/configuration/corsConfig.java
+++ src/main/java/com/weather/configuration/corsConfig.java
@@ -10,7 +10,7 @@ public class corsConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://polekm.github.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
